e61d98499c767f913a9ef0f6d499f588749cceab
sixayed <d.p.martyshkin@gmail.com>
2024-02-26 00:44:57 +0700

    Initial commit

---
+++ src/main/java/com/example/crackhash/CrackHashApplication.java
@@ -0,0 +1,13 @@
+package com.example.crackhash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackHashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrackHashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crackhash/CrackHashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crackhash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackHashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
